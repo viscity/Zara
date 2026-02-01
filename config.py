d665a9d76af70320e4d64c89e38526815a4c8fdd
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-02-01 20:55:15 +0530

    Change START_IMG_URL to new image sources
    Updated START_IMG_URL to use new image URLs.

--- config.py
+++ config.py
@@ -89,9 +89,9 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg",
-                 "https://te.legra.ph/file/c15d01b3e6b40ea141dc9.jpg",
-                 "https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/78yc1t.jpg",
+                 "https://files.catbox.moe/78yc1t.jpg",
+                 "https://files.catbox.moe/78yc1t.jpg"]
     
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/87f680aead03443f291b0.jpg"
